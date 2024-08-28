c63671847309b93b6f421cb951fd1b6296e92953
Willy Gutsche <willy.gutsche@unifesp.br>
2024-08-27 22:43:01 -0300

    wallet controller and post mapping to creation

---
+++ src/main/java/com/gutsche/picpay/application/controllers/WalletController.java
@@ -0,0 +1,28 @@
+package com.gutsche.picpay.application.controllers;
+
+import com.gutsche.picpay.application.services.WalletService;
+import com.gutsche.picpay.domain.dtos.CreationWalletDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/wallets")
+public class WalletController {
+  private final WalletService walletService;
+
+  @Autowired
+  public WalletController(WalletService walletService) {
+    this.walletService = walletService;
+  }
+
+  @PostMapping
+  public ResponseEntity<Void> create(@RequestBody CreationWalletDto creationWalletDto) {
+    walletService.create(creationWalletDto);
+    return ResponseEntity.status(HttpStatus.CREATED).build();
+  }
+}
