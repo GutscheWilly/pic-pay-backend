8afe8a2c626be75c7ce8dd0a041c5ce099144d9a
Willy Gutsche <willy.gutsche@unifesp.br>
2024-08-28 11:32:47 -0300

    permit any request for test in spring security config

--- src/main/java/com/gutsche/picpay/config/SpringSecurityConfig.java
+++ src/main/java/com/gutsche/picpay/config/SpringSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.gutsche.picpay.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,13 +12,17 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig {
+
+  @Value("${spring.h2.console.path}")
+  private String h2ConsolePath;
+
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
         .csrf(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(authorize -> authorize
-            .requestMatchers("/h2-console/**").permitAll()
-            .anyRequest().authenticated()
+            .requestMatchers(h2ConsolePath + "/**").permitAll()
+            .anyRequest().permitAll()
         )
         .headers(headers -> headers
             .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
